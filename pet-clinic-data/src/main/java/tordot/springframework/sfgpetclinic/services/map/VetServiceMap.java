2775c41352d8b1614b77c760b4e2211558dfe537
Boumedienne <cb_bouzidi@esi.dz>
2019-11-12 18:26:06 +0100

    Adding boostrap file .#14

---
+++ pet-clinic-data/src/main/java/tordot/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,36 @@
+package tordot.springframework.sfgpetclinic.services.map;
+
+
+import tordot.springframework.sfgpetclinic.model.Vet;
+import tordot.springframework.sfgpetclinic.services.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+         super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Vet object) {
+       super.delete(object);
+
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
