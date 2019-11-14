b0110ec38b611fc6c3e3fc9dc930354a978bd3da
Boumedienne <cb_bouzidi@esi.dz>
2019-11-14 15:46:25 +0100

    Adding boostrap file .#14

---
+++ pet-clinic-web/src/test/java/todot/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package todot.springframework.sfgpetclinic;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testng.annotations.Test;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
